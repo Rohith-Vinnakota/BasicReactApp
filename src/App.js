26fc5873f70d4cc573c05234a03bbc9a75c26177
vinnakota Rohith <rohithnani080@gmail.com>
2025-05-08 12:17:05 +0530

    moving the code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
 function App() {
+    const [number1, setNumber1] = useState();
+    const [number2, setNumber2] = useState();
+    const [total, setTotal] = useState(0);
+  
+    function calculateTotal() {
+      setTotal(number1 + number2);
+    }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h2>Adding Two Numbers</h2>
+      <input
+        placeholder="First Number"
+        type="number"
+        value={number1}
+        onChange={(e) => setNumber1(+e.target.value)}
+      />
+      <input
+        placeholder="Second Number"
+        type="number"
+        value={number2}
+        onChange={(e) => setNumber2(+e.target.value)}
+      />
+
+      <button onClick={calculateTotal}>Add Two Numbers</button>
+      <p>Total: {total || ""}</p>
+    
       </header>
+
     </div>
   );
 }
